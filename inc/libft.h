9b67dc25799c76ef2aa51210fc7e8f1bb165819d
Robert Celiowski <rceliows@shi-r10-p1.s19.be>
2025-12-10 16:23:19 +0100

    atol added

--- inc/libft.h
+++ inc/libft.h
@@ -4,6 +4,7 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
+# include <limits.h>
 
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 512
--- src/ft_atol.c
+++ src/ft_atol.c
@@ -1,28 +1,40 @@
 #include "../inc/libft.h"
 
+static int	ft_sign_and_skip(const char *nptr, size_t *i)
+{
+	int	sign;
+
+	sign = 1;
+	while ((nptr[*i] >= 9 && nptr[*i] <= 13) || nptr[*i] == 32)
+		(*i)++;
+	if (nptr[*i] == '-' || nptr[*i] == '+')
+	{
+		if (nptr[*i] == '-')
+			sign = -1;
+		(*i)++;
+	}
+	return (sign);
+}
+
 long	ft_atol(const char *nptr)
 {
-	size_t		i;
-	int		neg;
-	long	r;
+	size_t	i;
+	int		sign;
+	long	result;
+	int		digit;
 
 	i = 0;
-	r = 0;
-	neg = 1;
-	while (((nptr[i] >= 9 && nptr[i] <= 13) || nptr[i] == 32))
-		i++;
-	if (nptr[i] == 45 || nptr[i] == 43)
-	{
-		if (nptr[i] == 45)
-			neg = -1;
-		i++;
-	}
+	result = 0;
+	sign = ft_sign_and_skip(nptr, &i);
 	while (nptr[i] >= '0' && nptr[i] <= '9')
 	{
-		r = r * 10;
-		r = r + (nptr[i] - 48);
+		digit = nptr[i] - '0';
+		if (sign == 1 && result > (LONG_MAX - digit) / 10)
+			return (LONG_MAX);
+		if (sign == -1 && -result < (LONG_MIN + digit) / 10)
+			return (LONG_MIN);
+		result = result * 10 + digit;
 		i++;
 	}
-	r = r * neg;
-	return (r);
+	return (result * sign);
 }
